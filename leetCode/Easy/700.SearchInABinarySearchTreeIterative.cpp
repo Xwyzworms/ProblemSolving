f3ea62e297bfa1ae2395ea01846822c263080510
Xwyzworms <pratamaazmi1@gmail.com>
2023-05-01 20:34:05 +0700

    add solution

---
+++ leetCode/Easy/700.SearchInABinarySearchTreeIterative.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x ) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right ) : val(x), left(left), right(right){}
+};
+class Solution {
+    public:
+        TreeNode* searchBST(TreeNode* root, int val){
+
+            while(root!=NULL) 
+            {
+                if(root->val == val) return root;
+                else if( val > root->val) {
+                    root= root->right;
+                }
+                else if(val < root->val) {
+                    root = root->left;
+                }
+            }
+            return NULL;
+        } 
+
+};
